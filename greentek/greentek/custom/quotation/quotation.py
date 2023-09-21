161368109a1b0fe4ed2bad6cc7ea9b366c90ff0b
varshachavan2020 <83449264+varshachavan2020@users.noreply.github.com>
2023-09-21 10:24:16 +0530

    Update quotation.py

--- greentek/greentek/custom/quotation/quotation.py
+++ greentek/greentek/custom/quotation/quotation.py
@@ -12,9 +12,4 @@ import datetime
 from frappe.model.naming import make_autoname
 
 def autoname(doc,method):
-    fiscal_year = frappe.db.sql(""" Select name from `tabFiscal Year` WHERE %(posting_date)s between year_start_date and year_end_date """, {"posting_date":doc.posting_date}, as_dict=True,)
-    if doc.company ="Greentek Global Ventures Limited":
-        doc.name = make_autoname('GGVL-' + fiscal_year[0].get('name') + '-.#####')
-    elif doc.company="ACETEK MEP SOLUTIONS":
-        doc.name = make_autoname('AMS-' + fiscal_year[0].get('name') + '-.#####')
-    
+    pass
